9fa08d65dee1d6581cdb5c5ce3f8a5446d82525c
Touhidul Shawan <touhidulshawan@gmail.com>
2021-02-17 03:06:29 +0600

    add dracula color theme

--- rc.lua
+++ rc.lua
@@ -44,7 +44,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "gruvbox/theme.lua")
+--beautiful.init(gears.filesystem.get_themes_dir() .. "gruvbox/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "dracula/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"

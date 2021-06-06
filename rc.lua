6702f693edbe139fe3f161024b4b6dd7dfe1b1f9
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-06 15:17:13 +0600

    show border on client

--- rc.lua
+++ rc.lua
@@ -127,18 +127,18 @@ end
 screen.connect_signal("property::geometry", set_wallpaper)
 
 -- hide boder if there are only one client
-screen.connect_signal("arrange", function(s)
-local max = s.selected_tag.layout.name == "max"
-local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
--- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
-for _, c in pairs(s.clients) do
-   if (max or only_one) and not c.floating or c.maximized then
-      c.border_width = 0
-   else
-      c.border_width = beautiful.border_width
-   end
-end
-end)
+--screen.connect_signal("arrange", function(s)
+--local max = s.selected_tag.layout.name == "max"
+--local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
+---- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
+--for _, c in pairs(s.clients) do
+--   if (max or only_one) and not c.floating or c.maximized then
+--      c.border_width = 0
+--   else
+--      c.border_width = beautiful.border_width
+--   end
+--end
+--end)
 
 awful.screen.connect_for_each_screen(function(s)
 -- Wallpaper
